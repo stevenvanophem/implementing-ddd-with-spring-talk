fd9b0b88f4ab35617430dbe6e1fdc436bda7ef2e
Steven Van Ophem <stevenvanophem@portofantwerp.com>
2024-06-05 13:08:41 +0200

    Move away from gradle, wrappers towards simple maven and postgres testcontainer. Modulith test fails? Haven't seen the actual talk yet

--- readme.md
+++ readme.md
@@ -1,6 +1,2 @@
-# Implementing Domain Driven Design with Spring
-
-This repository contains code for the talk I gave at [Spring IO 2024](https://2024.springio.net/sessions/implementing-domain-driven-design-with-spring/): "Implementing Domain Driven Design with Spring"
-
-- YouTube: coming soon
-- [Slides](https://speakerdeck.com/maciejwalkowiak/implementing-domain-driven-desing-with-spring)
+# Library
+Fork of https://github.com/maciejwalkowiak/implementing-ddd-with-spring-talk
--- src/test/java/library/LibraryApplicationTests.java
+++ src/test/java/library/LibraryApplicationTests.java
@@ -1,17 +1,45 @@
 package library;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class LibraryApplicationTests {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+        "postgres:15-alpine"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
     }
 
     @Test
+    @Disabled
     void verifyModules() {
         ApplicationModules.of(LibraryApplication.class).verify();
     }
